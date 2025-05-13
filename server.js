004f7503cbb35c33ea119ec7e7d97f191afb3590
Lavanya-rao05 <lavanyarao0502@gmail.com>
2025-05-13 20:42:04 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const emailRoutes = require("./routes/emailRoutes");
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use("/", emailRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on port ${process.env.PORT}`)
+    );
+  })
+  .catch((err) => console.log(err));
